acff418ed98b7c849e3a3a1ada7c18d800c9c3a5
Arnaud Roussel <arnaud.roussel.rx@gmail.com>
2018-05-10 21:39:58 -0400

    Phone rotation partial support

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,35 @@
 // Cordova setup
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady);
 
 function onDeviceReady() {
 
 }
+
+// Global variables
+var boxWidth = window.innerWidth;
+var boxHeight = window.innerHeight;
+var boxDepth = window.innerWidth;
 
 // Three.js variables
 var scene, camera, renderer;
 
 // Cannon.js variables
 
+
+// Sensor data variables
+var beta = 0;
+var gamma = 0;
+window.addEventListener('deviceorientation', onDeviceOrientation);
+
+// Face tracking variables
+var distance = boxHeight * 2;
+
+function onDeviceOrientation(event) {
+  if (event.beta !== null)
+    beta = THREE.Math.degToRad(event.beta);
+  if (event.gamma !== null)
+    gamma = THREE.Math.degToRad(event.gamma);
+}
 
 initThree();
 initCannon();
@@ -20,9 +40,7 @@ function initThree() {
   scene = new THREE.Scene();
 
   // Camera
-  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight);
-  camera.position.x = window.innerWidth / 2;
-  camera.position.z = window.innerHeight * 2;
+  camera = new THREE.PerspectiveCamera(90, boxWidth / boxHeight);
 
   // Renderer
   renderer = new THREE.WebGLRenderer();
@@ -31,16 +49,16 @@ function initThree() {
   document.body.appendChild(renderer.domElement);
 
   // Box
-  var cubeGeometry = new THREE.BoxGeometry(window.innerWidth, window.innerHeight, window.innerWidth);
+  var cubeGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
   var cubeMaterial = new THREE.MeshStandardMaterial({color: 0x008800});
   cubeMaterial.side = THREE.BackSide;
   var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.position.z = -window.innerWidth / 2;
+  cube.position.z = -boxDepth / 2;
   scene.add(cube);
 
   // Lights
   var ambient = new THREE.AmbientLight(0xffffff, 0.4);
-  var point = new THREE.PointLight(0xffffff, 1, window.innerHeight);
+  var point = new THREE.PointLight(0xffffff, 1, boxHeight);
   scene.add(ambient);
   scene.add(point);
 }
@@ -53,6 +71,7 @@ function animate() {
   requestAnimationFrame(animate);
   updatePhysics();
   updateScene();
+  findCameraPosition();
   updateCamera();
   render();
 }
@@ -65,20 +84,30 @@ function updateScene() {
 
 }
 
+function findCameraPosition() {
+  camera.position.x = -distance * Math.sin(gamma);
+  camera.position.y = distance * Math.sin(beta);
+  camera.position.z = distance * Math.cos(beta) * Math.cos(gamma);
+}
+
 function updateCamera() {
   // Calculate the fov according to the distance from the phone
-  camera.fov = Math.atan((window.innerHeight / 2) / camera.position.z) / Math.PI * 180 * 2;
+  camera.fov = THREE.Math.radToDeg(Math.atan((boxHeight / 2) / camera.position.z)) * 2;
 
   // Update the projection matrix with the new fov
   camera.updateProjectionMatrix();
 
   // Set the oblique frustum to look at the box
   // Horizontal obliqueness
-  camera.projectionMatrix.elements[8] = -1;
+  camera.projectionMatrix.elements[8] = -camera.position.x / (boxWidth / 2);
   // Vertical obliqueness
-  camera.projectionMatrix.elements[9] = 0;
+  camera.projectionMatrix.elements[9] = -camera.position.y / (boxHeight / 2);
 }
 
 function render() {
   renderer.render(scene, camera);
+}
+
+function debug(text) {
+  document.getElementById('debug').innerHTML = text;
 }
