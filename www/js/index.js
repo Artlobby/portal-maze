531ba61a9051a7f4226568924209f502b3093516
Arnaud Roussel <arnaud.roussel.rx@gmail.com>
2018-05-10 23:47:32 -0400

    Cannon setup

--- www/js/index.js
+++ www/js/index.js
@@ -6,15 +6,17 @@ function onDeviceReady() {
 }
 
 // Global variables
-var boxWidth = window.innerWidth;
-var boxHeight = window.innerHeight;
-var boxDepth = window.innerWidth;
+var boxHeight = 111;
+var boxWidth = boxHeight * window.innerWidth / window.innerHeight;
+var boxDepth = boxWidth;
+var ballRadius = boxWidth / 20;
 
 // Three.js variables
-var scene, camera, renderer;
+var scene, camera, renderer, ballMesh;
 
 // Cannon.js variables
-
+var world, ballBody;
+var gravity = 9807;
 
 // Sensor data variables
 var beta = 0;
@@ -40,7 +42,7 @@ function initThree() {
   scene = new THREE.Scene();
 
   // Camera
-  camera = new THREE.PerspectiveCamera(90, boxWidth / boxHeight);
+  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight);
 
   // Renderer
   renderer = new THREE.WebGLRenderer();
@@ -49,12 +51,18 @@ function initThree() {
   document.body.appendChild(renderer.domElement);
 
   // Box
-  var cubeGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-  var cubeMaterial = new THREE.MeshStandardMaterial({color: 0x008800});
-  cubeMaterial.side = THREE.BackSide;
-  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.position.z = -boxDepth / 2;
-  scene.add(cube);
+  var boxGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  var boxMaterial = new THREE.MeshStandardMaterial({color: 0x008800});
+  boxMaterial.side = THREE.BackSide;
+  var box = new THREE.Mesh(boxGeometry, boxMaterial);
+  box.position.z = -boxDepth / 2;
+  scene.add(box);
+
+  // Ball
+  var ballGeometry = new THREE.SphereGeometry(ballRadius, 32, 32);
+  var ballMaterial = new THREE.MeshStandardMaterial({color: 0x888888});
+  ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+  scene.add(ballMesh);
 
   // Lights
   var ambient = new THREE.AmbientLight(0xffffff, 0.4);
@@ -64,7 +72,22 @@ function initThree() {
 }
 
 function initCannon() {
+  // World
+  world = new CANNON.World();
+  world.broadphase = new CANNON.NaiveBroadphase();
 
+  // Box
+  var boxShape = new CANNON.Box(new CANNON.Vec3(boxWidth / 2, boxHeight / 2, boxDepth));
+  world.addBody(new CANNON.Body({mass: 0, shape: boxShape, position: new CANNON.Vec3(0, boxHeight, 0)}));
+  world.addBody(new CANNON.Body({mass: 0, shape: boxShape, position: new CANNON.Vec3(boxWidth, 0, 0)}));
+  world.addBody(new CANNON.Body({mass: 0, shape: boxShape, position: new CANNON.Vec3(0, -boxHeight, 0)}));
+  world.addBody(new CANNON.Body({mass: 0, shape: boxShape, position: new CANNON.Vec3(-boxWidth, 0, 0)}));
+  world.addBody(new CANNON.Body({mass: 0, shape: boxShape, position: new CANNON.Vec3(0, 0, -boxDepth * 2)}));
+
+  // Ball
+  var ballShape = new CANNON.Sphere(ballRadius);
+  ballBody = new CANNON.Body({mass: 0.005, shape: ballShape});
+  world.addBody(ballBody);
 }
 
 function animate() {
@@ -77,7 +100,15 @@ function animate() {
 }
 
 function updatePhysics() {
-
+  world.gravity.set(gravity * Math.sin(gamma), -gravity * Math.sin(beta), -gravity * Math.cos(beta) * Math.cos(gamma));
+  world.step(1 / 60);
+  ballMesh.position.x = ballBody.position.x;
+  ballMesh.position.y = ballBody.position.y;
+  ballMesh.position.z = ballBody.position.z;
+  ballMesh.quaternion.x = ballBody.quaternion.x;
+  ballMesh.quaternion.y = ballBody.quaternion.y;
+  ballMesh.quaternion.z = ballBody.quaternion.z;
+  ballMesh.quaternion.w = ballBody.quaternion.w;
 }
 
 function updateScene() {
